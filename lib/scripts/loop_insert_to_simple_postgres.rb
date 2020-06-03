710471a6a57fdda4c6ec7f903cbce2c1214b3d45
tsutomu watanabe <watanabe162@gmail.com>
2020-06-03 15:17:06 +0900

    add create index to benchmark script

--- lib/scripts/loop_insert_to_simple_postgres.rb
+++ lib/scripts/loop_insert_to_simple_postgres.rb
@@ -21,6 +21,14 @@ insert_result = Benchmark.realtime do
 end
 puts "insert time of #{number} : #{insert_result.to_s}"
 
+# create index on id
+index_result = Benchmark.realtime do
+  result = connection.exec(
+      "CREATE INDEX prefix_ar_10_id_index ON prefix_ar_10(id);"
+  )
+end
+puts "create index time of #{number} : #{index_result.to_s}"
+
 # select
 select_result = Benchmark.realtime do
   number.times {|i|
@@ -30,3 +38,13 @@ select_result = Benchmark.realtime do
   }
 end
 puts "select time of #{number} : #{select_result.to_s}"
+
+# update
+update_result = Benchmark.realtime do
+  number.times {|i|
+    result = connection.exec(
+        "UPDATE prefix_ar_10 SET version='0.0.2' WHERE  accession = '#{accessions[i]}';"
+    )
+  }
+end
+puts "update time of #{number} : #{update_result.to_s}"
