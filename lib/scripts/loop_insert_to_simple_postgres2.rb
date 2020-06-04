2a7c59feb23fc22eb7c68a9704b9accd8319ab95
tsutomu watanabe <watanabe162@gmail.com>
2020-06-04 15:12:24 +0900

    transactionアリに変更

--- lib/scripts/loop_insert_to_simple_postgres2.rb
+++ lib/scripts/loop_insert_to_simple_postgres2.rb
@@ -3,44 +3,67 @@ require "bundler/setup"
 require 'benchmark'
 require 'pg'
 
-number = 1000000;
-
-insert_sql = ""
-select_sql = ""
-update_sql = ""
-number.times do |i|
-  accession = 'AR' + format('%08d', i)
-  insert_sql += "INSERT INTO prefix_ar_10 (accession, agent, version, status, visibility, created_at, updated_at) VALUES ('#{accession}', 'ddbj', '0.0.1', 'Public', true, now(), now());"
-  select_sql += "SELECT * FROM  prefix_ar_10 WHERE accession = '#{accession}';"
-  update_sql += "UPDATE prefix_ar_10 SET version='0.0.2' WHERE  accession = '#{accession}';"
-end
-
-
 connection = PG::connect(:host => "localhost", :user => "postgres", :password => "postgres", :dbname => "ddbj", :port => "15432")
+number = 1000000
 
-# create index on accession
-index_result = Benchmark.realtime do
-  connection.exec(
-      "CREATE UNIQUE INDEX accession_index ON prefix_ar_10 (accession);"
-  )
+connection.prepare('insert', "INSERT INTO prefix_ar_10 (accession, agent, version, status, visibility, created_at, updated_at) VALUES ($1, 'ddbj', '0.0.1', 'Public', true, now(), now())")
+connection.prepare('select', "SELECT * FROM  prefix_ar_10 WHERE accession = $1")
+connection.prepare('update', "UPDATE prefix_ar_10 SET version='0.0.2' WHERE  accession = $1")
+connection.prepare('update2', "UPDATE prefix_ar_10 SET version='0.0.3' WHERE  accession = $1")
+
+# insert with transaction
+insert_with_transaction_result = Benchmark.realtime do
+  connection.exec('BEGIN')
+  number.times do |i|
+    connection.exec_prepared('insert', ['AR' + format('%08d', i)])
+  end
+  connection.exec('COMMIT')
 end
-puts "create index time of #{number} : #{index_result.to_s}"
+puts "insert with transaction time of #{number} : #{insert_with_transaction_result.to_s}"
 
+# 一旦テーブルを空にする
+connection.exec('TRUNCATE TABLE prefix_ar_10 RESTART IDENTITY;')
 
-# insert
-insert_result = Benchmark.realtime do
-   connection.exec(insert_sql)
+# insert without transaction
+insert_without_transaction_result = Benchmark.realtime do
+  number.times do |i|
+    connection.exec_prepared('insert', ['AR' + format('%08d', i)])
+  end
 end
-puts "insert time of #{number} : #{insert_result.to_s}"
+puts "insert without transaction time of #{number} : #{insert_without_transaction_result.to_s}"
 
-# select
-select_result = Benchmark.realtime do
-  connection.exec(select_sql)
+# select with transaction
+select_with_transaction_result = Benchmark.realtime do
+  connection.exec('BEGIN')
+  number.times do |i|
+    connection.exec_prepared('select', ['AR' + format('%08d', i)])
+  end
+  connection.exec('COMMIT')
 end
-puts "select time of #{number} : #{select_result.to_s}"
+puts "select with transaction_time of #{number} : #{select_with_transaction_result.to_s}"
 
-# update
-update_result = Benchmark.realtime do
-  connection.exec(update_sql)
+# select without transaction
+select_with_transaction_result = Benchmark.realtime do
+  number.times do |i|
+    connection.exec_prepared('select', ['AR' + format('%08d', i)])
+  end
 end
-puts "update time of #{number} : #{update_result.to_s}"
+puts "select without transaction_time of #{number} : #{select_with_transaction_result.to_s}"
+
+# update with transaction
+update_with_transaction_result = Benchmark.realtime do
+  connection.exec('BEGIN')
+  number.times do |i|
+    connection.exec_prepared('update', ['AR' + format('%08d', i)])
+  end
+  connection.exec('COMMIT')
+end
+puts "update with transaction time of #{number} : #{update_with_transaction_result.to_s}"
+
+# update without transaction
+update_without_transaction_result = Benchmark.realtime do
+  number.times do |i|
+    connection.exec_prepared('update2', ['AR' + format('%08d', i)])
+  end
+end
+puts "update without transaction time of #{number} : #{update_without_transaction_result.to_s}"
