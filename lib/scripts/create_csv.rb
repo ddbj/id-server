f4a5835cc08905acc5955415e72d1b31268cb0b4
tsutomu watanabe <watanabe162@gmail.com>
2020-06-25 14:33:12 +0900

    change create_csv.rb

--- lib/scripts/create_csv.rb
+++ lib/scripts/create_csv.rb
@@ -9,12 +9,14 @@ CSV.open('../csv/accession_file.csv', 'w') do |csv|
   csv << ["accession", "agent", "version", "status", "visibility", "created_at", "updated_at"]
   # 1000万
   10000000.times do |num|
+    p num
     accession = PREFIX + format('%08d', num)
     if num.even?
       status = "Public"
     else
       status = "Private"
     end
+    sleep(Random.rand(0.1).round(3))
     csv << [accession, "ddbj", "0.0.1", status, true, Time.now, Time.now]
   end
 end
