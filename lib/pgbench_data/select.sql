9dbee050b1cf31cc8ad8f0f333854dfbd4d6786d
Tsutomu Watanabe <watanabe162@gmail.com>
2020-07-27 13:33:48 +0900

    add pgbench_data

---
+++ lib/pgbench_data/select.sql
@@ -0,0 +1,8 @@
+\set nbranches :scale
+\set ntellers 10 * :scale
+\set naccounts 100000 * :scale
+\set aid random(1, :naccounts)
+\set bid random(1, :nbranches)
+\set tid random(1, :ntellers)
+\set delta random(-5000, 5000)
+SELECT abalance FROM pgbench_accounts WHERE aid = :aid;
--- lib/scripts/create_csv.rb
+++ lib/scripts/create_csv.rb
@@ -4,19 +4,20 @@ require 'csv'
 
 PREFIX = "AR"
 
-CSV.open('../csv/accession_file.csv', 'w') do |csv|
+CSV.open('../csv/accession_file2.csv', 'w') do |csv|
 
   csv << ["accession", "agent", "version", "status", "visibility", "created_at", "updated_at"]
   # 1000万
   10000000.times do |num|
-    p num
+    #p num
+    num = num + 10000000
     accession = PREFIX + format('%08d', num)
     if num.even?
       status = "Public"
     else
       status = "Private"
     end
-    sleep(Random.rand(0.1).round(3))
-    csv << [accession, "ddbj", "0.0.1", status, true, Time.now, Time.now]
+    time = (Time.now + Random.rand(0.1).round(3)).strftime("%Y-%m-%d %H:%M:%S")
+    csv << [accession, "ddbj", "0.0.1", status, true, time, time]
   end
 end
